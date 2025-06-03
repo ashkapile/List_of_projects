a80cf757ba6868629a0c029fd80e23540ec61465
ashkapile <ashvinikapile123@gmail.com>
2025-06-03 13:14:14 +0530

    fetched API Token without exposing it

--- list_of_project.py
+++ list_of_project.py
@@ -1,15 +1,21 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-#import os
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 email = "ashvinikapile123@gmail.com"
 
 url = "https://ashvinikapile123.atlassian.net/rest/agile/1.0/board/1/issue"
 
-API_TOKEN = "JIRA_API_TOKEN"
+API_TOKEN = os.getenv("JIRA_API_TOKEN")
 
 auth = HTTPBasicAuth(email, API_TOKEN)
+
+if not API_TOKEN:
+    raise ValueError("Missing API Token in .env")
 
 headers = {
     "Accept": "application/json"
